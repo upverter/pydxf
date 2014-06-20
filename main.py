dbca9f68482a2883f6f419b43172b437a370b263
Alex Williams <alex@upverter.com>
2014-06-20 12:51:41 -0400

    Add HEADER section parsing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import collections
+import copy
 import Tkinter as tk
 
 
@@ -35,6 +36,12 @@ class DxfRecord(object):
             raise FormatException('Read group number <%s> is not a number.', group)
 
         return record
+
+    def is_section_end(self):
+        return self.code == 0 and self.value == 'ENDSEC'
+
+    def __repr__(self):
+        return 'DxfRecord<%s, %s>' % (self.code, self.value)
 
 
 class DxfEntity(object):
@@ -152,7 +159,7 @@ class DxfSection(object):
             raise FormatException('Sections must consist of at least a start record, name record, and end record')
         if records[1].code != 2:
             raise FormatException('Section records must be immediately followed by a section name record.')
-        if records[-1].code != 0 or records[-1].value != 'ENDSEC':
+        if not records[-1].is_section_end():
             raise FormatException('Section records must end with an end record.')
 
         if not DxfSection.section_factories:
@@ -165,6 +172,48 @@ class DxfSection(object):
         DxfSection.section_factories = collections.defaultdict(lambda: DxfSection.__make_default_section)
         for cls in DxfSection.__subclasses__():
             DxfSection.section_factories[cls.SECTION_TYPE] = cls.make_section
+
+
+class HeaderSection(DxfSection):
+
+    SECTION_TYPE = 'HEADER'
+
+    def __init__(self):
+        super(HeaderSection, self).__init__()
+        self.name = HeaderSection.SECTION_TYPE
+        self.variables = collections.defaultdict(lambda: None)
+
+    def get_variable(self, name):
+        return self.variables[name]
+
+    @staticmethod
+    def make_section(records):
+        section = HeaderSection()
+        building_variable = False
+        start_index = 0
+
+        for i, rec in enumerate(records[2:], 2):
+            if building_variable:
+                if rec.is_section_end() or rec.code == 9:
+                    section._add_variable(*HeaderSection._make_variable(records[start_index:i]))
+                    start_index = i
+            else:
+                if rec.code == 9:
+                    start_index = i
+                    building_variable = True
+                else:
+                    section.add_record(rec)
+
+        return section
+
+    def _add_variable(self, name, value):
+        self.variables[name] = value
+
+    @staticmethod
+    def _make_variable(records):
+        # Not doing data validation. Caller should make sure data is valid.
+        var_name = records[0].value.lstrip('$')
+        return var_name, records[1].value if len(records) == 2 else copy.deepcopy(records[1:])
 
 
 class EntitiesSection(DxfSection):
@@ -226,7 +275,7 @@ class DxfFile(object):
         for rec in records:
             if building_section_records:
                 section_records.append(rec)
-                if rec.code == 0 and rec.value == 'ENDSEC':
+                if rec.is_section_end():
                     building_section_records = False
                     dxf_file.sections.append(DxfSection.make_section(section_records))
             else:
@@ -296,9 +345,11 @@ if __name__ == '__main__':
 
     fi = open('drawing1.dxf', 'rt')
     df = DxfFile.make_file(DxfFile.ascii_record_iterator(fi))
+    print 'Version: %s' % df.get_section('HEADER').get_variable('ACADVER')
+
     # df = DxfFile(fi)
     for section in df.iter_sections():
-        print section.name
+        # print section.name
         for rec in section.iter_records():
             pass
             # print '\t%s %s' % (rec.code, rec.value)
